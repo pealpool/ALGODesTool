203c1038a6449995b6813774d2fe8422218d0bff
pealpool <pealpool@gmail.com>
2020-12-21 17:54:09 +0800

    ok

--- js/index.js
+++ js/index.js
@@ -1,4 +1,9 @@
 let $dp = document.getElementById('getBox');
+let imgData;
+let myFileName;
+let myHtmlData_1 = '<!doctype html><html><head><meta charset="utf-8"><meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no"><title>设计稿</title><script type="text/javascript">        function scrollWindow() {            let w = 960 - document.body.clientWidth / 2;            window.scrollTo(w, 0);        }        document.oncontextmenu = new Function("return false;");        document.onkeydown = document.onkeyup = document.onkeypress = function (e) {            e = event || window.event || arguments.callee.caller.arguments[0];            if (e.keyCode === 123) {                e.returnValue = false;                return false;            }        };        document.documentElement.addEventListener(\'touchstart\', function (event) {            if (event.touches.length > 1) {                event.preventDefault();            }        }, false);        let lastTouchEnd = 0;        document.documentElement.addEventListener(\'touchend\', function (event) {            let now = Date.now();            if (now - lastTouchEnd<= 300) {                event.preventDefault();            }            lastTouchEnd = now;        }, false);</script><style>        img {            pointer-event: none;            -webkit-user-select: none;            -moz-user-select: none;            user-select: none;        }</style></head><body style="margin:0; padding:0" onload="scrollWindow()"><img src="';
+let myHtmlData_2 = '"/></body></html>';
+
 $dp.addEventListener('dragover', function (e) {
     e.stopPropagation();
     //阻止浏览器默认打开文件的操作
@@ -13,41 +18,50 @@ $dp.addEventListener("drop", function (e) {
     let files = e.dataTransfer.files;
     let file = files[0];
     let imgFile = new FileReader();
+    myFileName = getFileName(file.name);
     imgFile.readAsDataURL(file);
     imgFile.onload = function () {
-        let imgData = this.result; //base64数据
-        console.log(imgData);
+        imgData = this.result; //base64数据
+        // console.log(imgData);
+        overLook(imgData);
     }
 });
 
-function changeFrameHeight() {
-    let ifm = document.getElementById("myFrame");
-    ifm.height = document.documentElement.clientHeight;
+function getFileName(name) {
+    return name.substring(0, name.lastIndexOf("."))
 }
 
-window.onresize = function () {
-    changeFrameHeight();
+function overLook(imgData) {
+    let $img = document.createElement("img");
+    $img.id = 'myImg';
+    $img.src = imgData;
+    document.body.appendChild($img);
+    $('#getBox').hide();
 }
 
-let $myFm = document.createElement("iframe");
-$myFm.src = "翼彩-首页设计稿.html";
-$myFm.frameBorder = 0;
-$myFm.id='myFrame';
-
-if ($myFm.attachEvent){
-    $myFm.attachEvent("onload", function(){
-        alert("Local iframe is now loaded.");
-    });
-} else {
-    $myFm.onload = function(){
-        console.log("Local iframe is now loaded.");
-        $("#myFrame").scrollLeft(400);//todo 无效
-    };
-}
-document.body.appendChild($myFm);
-
-
-$(document).on('click','#reBox',function () {
-   $('#myFrame').remove();
-   $('#getBox').show();
+$(document).on('click', '#reBox', function () {
+    $('#myImg').remove();
+    $('#getBox').show();
 });
+
+
+$(document).on('click', '#downloadBox', function () {
+    let myHtmlData = myHtmlData_1 + imgData + myHtmlData_2;
+    let funDownload = function (content, filename) {
+        let eleLink = document.createElement('a');
+        eleLink.download = filename;
+        eleLink.style.display = 'none';
+        // 字符内容转变成blob地址
+        let blob = new Blob([content]);
+        eleLink.href = URL.createObjectURL(blob);
+        // 触发点击
+        document.body.appendChild(eleLink);
+        eleLink.click();
+        // 然后移除
+        document.body.removeChild(eleLink);
+    }
+    funDownload(myHtmlData, myFileName + '设计稿.html');
+});
+
+
+//todo reset也要省了吧
